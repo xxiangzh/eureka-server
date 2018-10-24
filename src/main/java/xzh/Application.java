82962b389a8027d2baa8e63151d7aeae5451e641
xzh <anenan@qq.com>
2018-10-24 15:03:15 +0800

    first commit

---
+++ src/main/java/xzh/Application.java
@@ -0,0 +1,19 @@
+package xzh;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 向振华
+ * @date 2018/10/23 17:00
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
