22c3f2ab49f98aaa2ab3a7b9ee6180349628b94b
xzh <anenan@qq.com>
2019-05-17 09:55:32 +0800

    升级到Finchley.RELEASE

--- src/main/java/com/xzh/eureka/Application.java
+++ src/main/java/com/xzh/eureka/Application.java
@@ -1,7 +1,7 @@
-package xzh;
+package com.xzh.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -11,9 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
